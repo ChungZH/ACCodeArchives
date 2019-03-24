d451cce42109c5f9b46d6faf78913d19fdfb72a6
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-03-24 18:14:40 +0800

    :tada: Add P1138.cpp

---
+++ Luogu/P1138.cpp
@@ -0,0 +1,22 @@
+// [P1138 第k小整数] https://www.luogu.org/problemnew/show/P1138
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a, a+n);
+    int ans = unique(a, a+n) - a;
+    if(k < ans)
+    {
+        cout<<a[k-1];
+    } else {
+        cout<<"NO RESULT" << endl;
+    } 
+    return 0;
+}
