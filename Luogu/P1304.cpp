c15a2725b2f3a2ea5cd06c483a0295c22ac5c970
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-04-10 17:22:45 +0800

    :gem: Add P1304.cpp

---
+++ Luogu/P1304.cpp
@@ -0,0 +1,34 @@
+// [P1304 哥德巴赫猜想] https://www.luogu.org/problemnew/show/P1304
+#include <bits/stdc++.h>
+using namespace std;
+bool isPrime(int a)
+{
+    if (a == 2) return 1;
+    for (int i = 2; i < a; i++)
+    {
+        if (a % i == 0)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 4; i <= n; i+=2)
+    {
+        for (int j = 2; j <= i; j++)
+        {
+            int b = i - j;
+            
+            if (b + j == i && isPrime(b) == 1 && isPrime(j) == 1)
+            {
+                cout << i << "=" << j << "+" << b << endl;
+                break;
+            }
+        }
+    }
+    return 0;
+}
