bb2f55731f87cdd04cfa4b9f7da73405f39aaa96
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-04-12 17:14:00 +0800

    :ribbon: Add CF791A.cpp

---
+++ CodeForces/CF791A.cpp
@@ -0,0 +1,21 @@
+// [CF791A Bear and Big Brother] https://www.luogu.org/problemnew/show/CF791A
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int a, b, answer = 0;
+    cin >> a >> b;
+    
+    while (a <= b)
+    {
+        a *= 3; b *= 2;
+        answer++;
+    }
+    
+    cout << answer << endl;
+
+    return 0;
+}
