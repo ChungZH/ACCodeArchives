d82569b4f5918a7226cfdd97f1154c3d4e3b8011
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-04-17 16:39:44 +0800

    :gem: Add SP27561.cpp

---
+++ SPOJ/SP27561.cpp
@@ -0,0 +1,15 @@
+// [SP27561 GDCOFTI - Greatest Common Divisor Of Three Integers] https://www.luogu.org/problemnew/show/SP27561
+#include <bits/stdc++.h>
+using namespace std;
+int gcd(int x, int y);
+int main()
+{
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << gcd(a, gcd(b, c)) << endl;
+	return 0;
+}
+int gcd(int x, int y)
+{
+	return x % y ? gcd(y, x % y):y;
+}
