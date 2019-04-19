b0490067b968a7833be0abc3969a0dafabcbcec2
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-04-19 16:35:44 +0800

    :gem: Add CF758A.cpp

---
+++ CodeForces/CF758A.cpp
@@ -0,0 +1,19 @@
+// [CF758A Holiday Of Equality] https://www.luogu.org/problemnew/show/CF758A
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, temp, maxi = 0, sum = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> temp;
+        if (temp > maxi) maxi = temp;
+        sum += temp;
+    }
+
+
+    cout << maxi *n - sum << endl;
+
+    return 0;
+}
