34d7d53772165a903994da71b99b4ba459a4073a
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-04-20 17:45:16 +0800

    :gem: Add CF409D.cpp

---
+++ CodeForces/CF409D.cpp
@@ -0,0 +1,10 @@
+// [CF409D Big Data] https://www.luogu.org/problemnew/show/CF409D
+#include <iostream>  
+std::string s = "1001010111001010";  
+int main() 
+{  
+    int n;  
+    std::cin >> n;
+    std::cout << s[n-1] << std::endl;
+    return 0;  
+}  
