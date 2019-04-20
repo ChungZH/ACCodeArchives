84a6c0a5c894dd5281e76693ad09457d70c202b4
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-04-20 12:15:15 +0800

    :gem: Add AT667.cpp

---
+++ AtCoder/AT667.cpp
@@ -0,0 +1,15 @@
+// [AT667 天下一人力比較] https://www.luogu.org/problemnew/show/AT667
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string s[1000000];
+
+int main()
+{
+    int temp = 0;
+    while (cin >> s[temp++])
+    sort(s, s+temp);
+    cout << s[6] << endl;
+    return 0;
+}
