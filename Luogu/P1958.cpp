756c6d265d3a14700c930121795466de91edd6de
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-04-22 14:23:02 +0800

    :gem: Add DP code into P1958.cpp

--- Luogu/P1958.cpp
+++ Luogu/P1958.cpp
@@ -1,4 +1,7 @@
 // [P1958 上学路线_NOI导刊2009普及（6）] https://www.luogu.org/problemnew/show/P1958
+
+// DFS: 
+
 #include <iostream>
 #include <cstring>
 
@@ -54,4 +57,42 @@ void dfs(int x, int y)
 		dfs(x+1, y);
 	}
 	
+}
+
+// dp:
+
+#include <iostream>
+
+using namespace std;
+
+int a, b, n, ans;
+bool flag[50][50];
+int dp[50][50]; 
+
+int main()
+{
+	cin >> a >> b >> n;
+	
+	int x, y;
+	
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> x >> y;
+		flag[x][y] = true;
+	}
+	
+	dp[1][1] = 1;
+	
+	for (int i = 1; i <= a; i++)
+	{
+		for (int j = 1; j <= b; j++)
+		{
+			if (i == 1 && j == 1 || flag[i][j] == true) continue;
+			
+			dp[i][j] = dp[i-1][j] + dp[i][j-1];
+		}
+	}
+	
+	cout << dp[a][b] << endl;
+	return 0;
 }
