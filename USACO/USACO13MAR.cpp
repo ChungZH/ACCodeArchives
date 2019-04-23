d958bd62ed01c24629ef24352c2f6ad2f5db5a1c
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-04-23 17:15:08 +0800

    :gem: Add USACO13MAR.cpp

---
+++ USACO/USACO13MAR.cpp
@@ -0,0 +1,27 @@
+// [[USACO13MAR]扑克牌型Poker Hands] https://www.luogu.org/problemnew/show/P3078
+#include <bits/stdc++.h>
+using namespace std;
+
+long long n, now, ans;
+
+int main()
+{
+    cin >> n;
+    
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+        
+        if (a > now)
+        {
+            ans += a - now;
+        }
+        
+        now = a;
+    }
+    
+    cout << ans << endl;
+    
+    return 0; 
+}
