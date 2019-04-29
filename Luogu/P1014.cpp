819b9c808e2f95acf985d7311d498883020aa086
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-04-29 18:50:12 +0800

    :gem: Add P1014.cpp

---
+++ Luogu/P1014.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int i = 0, s = 0;
+
+    while (n > s)
+    {
+        s += ++i;
+    }
+
+    if (i % 2 == 1)
+    {
+        cout << s-n+1 << '/' << i+n-s << endl;
+    } else {
+        cout << i+n-s << '/' << s-n+1 << endl;
+    }
+
+    return 0;
+}
