739d980d4a9798a533ac114342fed64aa72a7557
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-05-15 14:27:25 +0800

    :gem: Add CF129A.cpp

---
+++ CodeForces/CF129A.cpp
@@ -0,0 +1,29 @@
+// [CF129A Cookies] https://www.luogu.org/problemnew/show/CF129A
+#include <iostream>
+using namespace std;
+
+int n, sum, ans;
+
+int main()
+{
+    cin >> n;
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sum += a[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        if ((sum - a[i]) % 2 == 0)
+        {
+            ans++;
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
