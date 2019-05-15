46169a2bb5f2e823e076ca8863a7a9c3df78c529
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-05-15 16:46:45 +0800

    :gem: Add CF919B.cpp

---
+++ CodeForces/CF919B.cpp
@@ -0,0 +1,32 @@
+// [CF919B Perfect Number] https://www.luogu.org/problemnew/show/CF919B
+#include <iostream>
+using namespace std;
+
+bool isPerfect(int n)
+{
+    int temp = 0;
+    while (n > 0)
+    {
+        temp += n % 10;
+        n /= 10;
+    }
+    if (temp == 10) return 1;
+    return 0;
+}
+
+int main()
+{
+    int k, count = 0;
+    cin >> k;
+
+    for (int i = 19; ; i++)
+    {
+        if (isPerfect(i)) count++;
+        if (count == k) 
+        {
+            cout << i << endl;
+            break;
+        }
+    }
+    return 0;
+}
