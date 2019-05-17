e4cd22a77fa58cdc12eb30abdb3b47da28269017
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-05-17 16:51:15 +0800

    :gem: Add CF711A.cpp

---
+++ CodeForces/CF711A.cpp
@@ -0,0 +1,49 @@
+// [CF711A Bus to Udayland] https://www.luogu.org/problemnew/show/CF711A
+#include <iostream>
+
+using namespace std;
+
+int n;
+bool flag, flag1;
+
+int main()
+{
+    cin >> n;
+    
+    char c[n*2][2], tempChar;
+    
+    for (int i = 0; i < n*2; i++)
+    {
+        cin >> c[i][0] >> c[i][1];
+        if (i % 2 == 0)
+        {
+            cin >> tempChar;
+        }
+        if (c[i][0] == 'O' && c[i][1] == 'O')
+        {
+            flag = true;
+            if (!flag1)
+                c[i][0] = c[i][1] = '+', flag1 = true;
+        }
+    }
+    
+    if (flag)
+    {
+        cout << "YES" << endl;
+        for (int i = 0; i < n*2; i++)
+        {
+            cout << c[i][0] << c[i][1];
+            if (i % 2 == 0)
+            {
+                cout << '|';				
+            } else {
+                cout << endl;
+            }
+        }	
+    } else {
+        cout << "NO" << endl;
+    }
+    
+    
+    return 0;
+}
