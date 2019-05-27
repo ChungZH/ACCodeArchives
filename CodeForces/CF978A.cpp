46539276de2f12416a381dadbad1897ce996c282
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-05-27 14:23:51 +0800

    :gem: Add CF978A.cpp

---
+++ CodeForces/CF978A.cpp
@@ -0,0 +1,30 @@
+// [CF978A Remove Duplicates] https://www.luogu.org/problemnew/show/CF978A
+#include <cstdio>
+#include <algorithm>
+
+int ans;
+int b[51], c[1000];
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int a[51];
+    
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &a[i]);
+
+    for (int i = n; i > 0; i--)
+    {
+    	c[a[i]]++;
+    	if (c[a[i]] == 1) b[++ans] = a[i];
+    }
+    
+    printf("%d\n", ans);
+    for (int i = ans; i > 0; i--)
+    {
+        printf("%d ", b[i]);
+    }
+    return 0;
+}
