f80038e940b7be870b5033f6e5a5f9d0b41af894
ChungZH <124694738@qq.com>
2019-05-31 19:25:19 +0800

    :gem: Add sources

---
+++ Luogu/P1219.cpp
@@ -0,0 +1,54 @@
+// [P1219 八皇后] https://www.luogu.org/problemnew/show/P1219
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int n, ans, a[14];
+bool b[14], c[30], d[30];
+
+void dfs(int step);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+
+    cin >> n;
+
+    dfs(1);
+
+    cout << ans << '\n';
+
+    return 0;
+}
+
+void dfs(int step)
+{
+    if (step == n+1)
+    {
+        ans++;
+
+        if (ans <= 3)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                cout << a[i] << ' ';
+            }
+            cout << '\n';
+        }
+
+        return;
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        if ((!b[i]) && (!c[step+i]) && (!d[step-i+n]))
+        {
+            a[step] = i;
+            b[i] = c[step+i] = d[step-i+n] = 1;
+            dfs(step+1);
+            a[step] = 0;
+            b[i] = c[step+i] = d[step-i+n] = 0;
+        }
+    }
+}
