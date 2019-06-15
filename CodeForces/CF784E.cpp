145626c49ca71597bc1e3485d4ab0d92ee893d1a
Haoshen Zhong <42088872+ChungZH@users.noreply.github.com>
2019-06-15 14:16:40 +0800

    :gem: Add CF784E.cpp

---
+++ CodeForces/CF784E.cpp
@@ -0,0 +1,12 @@
+// [CF784E Twisted Circuit] https://www.luogu.org/problemnew/show/CF784E
+#include <iostream>
+using namespace std;
+
+int iIn1, iIn2, iIn3, iIn4;
+
+int main()
+{
+    cin >> iIn1 >> iIn2 >> iIn3 >> iIn4;
+    cout << (((iIn1^iIn2)&(iIn3|iIn4))^((iIn2&iIn3)|(iIn1^iIn4))) << endl;
+    return 0;
+}
