1e1c5662264e3a050473b3dc84cf1477a3ede998
ChungZH <124694738@qq.com>
2019-07-19 17:18:52 +0800

    :gem: Add sources

---
+++ AtCoder/AT1753.cpp
@@ -0,0 +1,12 @@
+// [AT1753 ニコニコ数] https://www.luogu.org/problemnew/show/AT1753
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << n/25 << endl;
+    return 0;
+}
