1de21e588a77c8038994b73f4f6239a5d363f644
Flex Zhong <chungzh07@gmail.com>
2020-09-18 13:38:51 +0800

    Create P1062.cpp

---
+++ codes/Luogu/P1062.cpp
@@ -0,0 +1,25 @@
+// [P1062 数列] https://www.luogu.com.cn/problem/P1062 #进制
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int k, n;
+	cin >> k >> n;
+	
+	int a[1000000];
+	int i = 0;
+	while (n) {
+		a[++i] = n % 2;
+		n /= 2;
+	}
+	
+	long long ans = 0;
+	for (int j = i; j >= 0; j--) {
+		ans += a[j] * pow(k, j-1);
+	}
+	
+	cout << ans << endl;
+	return 0;
+} 
